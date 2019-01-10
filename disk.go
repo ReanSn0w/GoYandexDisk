34c2c3db741e160f38f82a665e085b7a90fc4264
ReanSn0w <re@nsnow.xyz>
2019-01-10 23:12:59 +0300

    Пока оставил только то, что нужно мне

--- disk.go
+++ disk.go
@@ -2,7 +2,10 @@ package GoYandexDisk
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"net/url"
 )
 
 const baseURL = "https://cloud-api.yandex.net/v1/disk/"
@@ -12,369 +15,316 @@ type YandexDisk struct {
 	accessToken string
 }
 
-func ConnectDisk(token string) YandexDisk{
+func ConnectDisk(token string) YandexDisk {
 	return YandexDisk{token}
 }
 
-func (yd YandexDisk)DiskStatus() (Disk, error){
-	resp, err := yd.createGETRequest("")
-	if err != nil {
-		return Disk{}, err
-	}
-
-	var body []byte
-	_, err = resp.Body.Read(body)
+func (yd *YandexDisk) DiskStatus() (Disk, error) {
+	body, err := yd.createRequest(
+		"",
+		"GET",
+		"",
+		nil,
+	)
 	if err != nil {
 		return Disk{}, err
 	}
 
 	var result Disk
-	err = json.Unmarshal(body, &result)
+	err = json.NewDecoder(body).Decode(&result)
 	return result, err
 }
 
 //Get Получение ресурса списка ресурсов
-func (yd YandexDisk)Get(path string, limit int, offset int) (ResourceList, error) {
-	link := "resources?path=" + path + "&limit=" + string(limit) + "&offset=" + string(offset)
-	resp, err := yd.createGETRequest(link)
+func (yd YandexDisk) Get(path string, limit int, offset int) (Resource, error) {
+	body, err := yd.createRequest(
+		"resources",
+		"GET",
+		createParamsMap(map[string]string{
+			"path": path,
+		}),
+		nil,
+	)
 	if err != nil {
-		return ResourceList{}, err
+		return Resource{}, err
 	}
 
-	var body []byte
-	_, err = resp.Body.Read(body)
-	if err != nil {
-		return ResourceList{}, err
-	}
-
-	var result ResourceList
-	err = json.Unmarshal(body, &result)
+	var result Resource
+	err = json.NewDecoder(body).Decode(&result)
 	return result, err
 }
 
-//FlatList Получение плоского списка файлов
-func (yd YandexDisk)FlatList(limit int, mediatype []string, offset int) (FileResourceList, error) {
-	//TODO добавить в запрос mediatype
-	link := "resources/files?limit=" + string(limit) + "&offset=" + string(offset)
-	resp, err := yd.createGETRequest(link)
-	if err != nil {
-		return FileResourceList{}, err
-	}
-
-	var body []byte
-	_, err = resp.Body.Read(body)
-	if err != nil {
-		return FileResourceList{}, err
-	}
-
-	var result FileResourceList
-	err = json.Unmarshal(body, &result)
-	return result, err
-}
-
-//LastUploaded получение списка последних загруженных файлов
-func (yd YandexDisk)LastUploaded(limit int, mediatype []string) (LastUploadedResourceList, error) {
-	//TODO добавить в запрос mediatype
-	link := "resources/files?limit=" + string(limit)
-	resp, err := yd.createGETRequest(link)
-	if err != nil {
-		return LastUploadedResourceList{}, err
-	}
-
-	var body []byte
-	_, err = resp.Body.Read(body)
-	if err != nil {
-		return LastUploadedResourceList{}, err
-	}
-
-	var result LastUploadedResourceList
-	err = json.Unmarshal(body, &result)
-	return result, err
-}
-
-//UploadFile
-//func (yd YandexDisk)UploadFile() {}
+////FlatList Получение плоского списка файлов
+//func (yd YandexDisk)FlatList(limit int, mediatype []string, offset int) (FileResourceList, error) {
+//	//TODO добавить в запрос mediatype
+//	link := "resources/files?limit=" + string(limit) + "&offset=" + string(offset)
+//	body, err := yd.createRequest(link, "GET")
+//	if err != nil {
+//		return FileResourceList{}, err
+//	}
+//
+//	var result FileResourceList
+//	err = json.NewDecoder(body).Decode(&result)
+//	return result, err
+//}
+//
+////LastUploaded получение списка последних загруженных файлов
+//func (yd YandexDisk)LastUploaded(limit int, mediatype []string) (LastUploadedResourceList, error) {
+//	//TODO добавить в запрос mediatype
+//	link := "resources/files?limit=" + string(limit)
+//	body, err := yd.createRequest(link, "GET")
+//	if err != nil {
+//		return LastUploadedResourceList{}, err
+//	}
+//
+//	var result LastUploadedResourceList
+//	err = json.NewDecoder(body).Decode(&result)
+//	return result, err
+//}
+//
+////UploadFile
+////func (yd YandexDisk)UploadFile() {}
 
 //UploadFileFromNet Загрузка файла на диск по ссылке из интернета
-func (yd YandexDisk)UploadFileFromNet(url string, path string, fields []string, disableRedirects bool) (Link, error){
-	link := "resources/upload?url=" + url + "?path=" + path
-	resp, err := yd.createPOSTRequest(link)
-
-	var body []byte
-	_, err = resp.Body.Read(body)
+func (yd YandexDisk) UploadFileFromNet(url string, path string, fields []string, disableRedirects bool) (Link, error) {
+	body, err := yd.createRequest(
+		"resources/upload",
+		"POST",
+		createParamsMap(map[string]string{
+			"url":  url,
+			"path": path,
+		}),
+		nil,
+	)
 	if err != nil {
 		return Link{}, err
 	}
 
 	var result Link
-	err = json.Unmarshal(body, &result)
+	err = json.NewDecoder(body).Decode(&result)
 	return result, err
 }
 
-//DownloadFile вернет объект Link со ссылкой для загрузки
-func (yd YandexDisk)DownloadFile(path string) (Link, error){
-	link := "resources/download?path=" + path
-	resp, err := yd.createGETRequest(link)
-	if err != nil {
-		return Link{}, err
-	}
-
-	var body []byte
-	_, err = resp.Body.Read(body)
-	if err != nil {
-		return Link{}, err
-	}
-
-	var result Link
-	err = json.Unmarshal(body, &result)
-	return result, err
-}
-
-//CopyResource копирование ресурса
-func (yd YandexDisk)CopyResource(from string, path string, owerwrite bool) (Link, error){
-	link := "resources/copy?from=" + from + "&path=" + path
-	resp, err := yd.createGETRequest(link)
-	if err != nil {
-		return Link{}, err
-	}
-
-	var body []byte
-	_, err = resp.Body.Read(body)
-	if err != nil {
-		return Link{}, err
-	}
-
-	var result Link
-	err = json.Unmarshal(body, &result)
-	return result, err
-}
-
-//ReplaceResource перемещение ресурса
-func (yd YandexDisk)ReplaceResource(from string, path string, owerwrite bool) (Link, error){
-	link := "resources/move?from=" + from + "&path=" + path
-	resp, err := yd.createGETRequest(link)
-	if err != nil {
-		return Link{}, err
-	}
-
-	var body []byte
-	_, err = resp.Body.Read(body)
-	if err != nil {
-		return Link{}, err
-	}
-
-	var result Link
-	err = json.Unmarshal(body, &result)
-	return result, err
-}
-
-//DeleteResource удаление ресурса
+////DownloadFile вернет объект Link со ссылкой для загрузки
+//func (yd YandexDisk)DownloadFile(path string) (Link, error){
+//	link := "resources/download?path=" + path
+//	body, err := yd.createRequest(link, "GET")
+//	if err != nil {
+//		return Link{}, err
+//	}
+//
+//	var result Link
+//	err = json.NewDecoder(body).Decode(&result)
+//	return result, err
+//}
+//
+////CopyResource копирование ресурса
+//func (yd YandexDisk)CopyResource(from string, path string, owerwrite bool) (Link, error){
+//	//TODO дописать
+//	link := "resources/copy?from=" + from + "&path=" + path
+//	body, err := yd.createRequest(link, "GET")
+//	if err != nil {
+//		return Link{}, err
+//	}
+//
+//	var result Link
+//	err = json.NewDecoder(body).Decode(&result)
+//	return result, err
+//}
+//
+////ReplaceResource перемещение ресурса
+//func (yd YandexDisk)ReplaceResource(from string, path string, owerwrite bool) (Link, error){
+//	link := "resources/move?from=" + from + "&path=" + path
+//	body, err := yd.createRequest(link, "GET")
+//	if err != nil {
+//		return Link{}, err
+//	}
+//
+//	var result Link
+//	err = json.NewDecoder(body).Decode(&result)
+//	return result, err
+//}
+//
+////DeleteResource удаление ресурса
 //func (yd YandexDisk)DeleteResource(path string, permanently bool) (Link, error){
 //	//TODO дописать
 //	link := "resources?path=" + path
-//	resp, err := yd.createGETRequest(link)
-//	if err != nil {
-//		return Link{}, err
-//	}
-//
-//	var body []byte
-//	_, err = resp.Body.Read(body)
+//	body, err := yd.createRequest(link, "DELETE")
 //	if err != nil {
 //		return Link{}, err
 //	}
 //
 //	var result Link
-//	err = json.Unmarshal(body, &result)
+//	err = json.NewDecoder(body).Decode(&result)
 //	return result, err
 //}
-
+//
+////CreateFolder создает папке на диске по указанному пути
 //func (yd YandexDisk)CreateFolder(path string) (Link, error){
 //	link := "resources/move?path=" + path
-//	resp, err := yd.createGETRequest(link)
-//	if err != nil {
-//		return Link{}, err
-//	}
-//
-//	var body []byte
-//	_, err = resp.Body.Read(body)
+//	body, err := yd.createRequest(link, "PUT")
 //	if err != nil {
 //		return Link{}, err
 //	}
 //
 //	var result Link
-//	err = json.Unmarshal(body, &result)
+//	err = json.NewDecoder(body).Decode(&result)
 //	return result, err
 //}
 
 //PublishResource публикация ресурса
-func (yd YandexDisk)PublishResource(path string) (Link, error){
-	link := "resources/publish?path=" + path
-	resp, err := yd.createGETRequest(link)
-	if err != nil {
-		return Link{}, err
-	}
-
-	var body []byte
-	_, err = resp.Body.Read(body)
-	if err != nil {
-		return Link{}, err
-	}
-
-	var result Link
-	err = json.Unmarshal(body, &result)
-	return result, err
-}
-
-//UnpublishResource отказаться от публикации ресурса
-func (yd YandexDisk)UnpublishResource(publickey string) (Resource, error){
-	//TODO доделать функцию
-	link := "public/resources" + publickey
-	resp, err := yd.createGETRequest(link)
-	if err != nil {
-		return Resource{}, err
-	}
-
-	var body []byte
-	_, err = resp.Body.Read(body)
-	if err != nil {
-		return Resource{}, err
-	}
-
-	var result Resource
-	err = json.Unmarshal(body, &result)
-	return result, err
-}
-
-//PublicResourceMeta получение мета информации о публичном ресурсе
-func (yd YandexDisk)PublicResourceMeta(publikKey string) (Resource, error){
-	link := "public/resources?public_key=" + publikKey
-	resp, err := yd.createGETRequest(link)
-	if err != nil {
-		return Resource{}, err
-	}
-
-	var body []byte
-	_, err = resp.Body.Read(body)
-	if err != nil {
-		return Resource{}, err
-	}
-
-	var result Resource
-	err = json.Unmarshal(body, &result)
-	return result, err
-}
-
-//DownloadPublicResource получение ссылки на зугрузку публичного ресурса
-func (yd YandexDisk)DownloadPublicResource(publikKey string) (Link, error){
-	link := "public/resources/download?public_key=" + publikKey
-	resp, err := yd.createGETRequest(link)
-	if err != nil {
-		return Link{}, err
-	}
-
-	var body []byte
-	_, err = resp.Body.Read(body)
+func (yd YandexDisk) PublishResource(path string) (Link, error) {
+	link := "resources/publish" + "?path=" + path
+	body, err := yd.createRequest(
+		link,
+		"PUT",
+		"",
+		nil,
+	)
 	if err != nil {
 		return Link{}, err
 	}
 
 	var result Link
-	err = json.Unmarshal(body, &result)
+	err = json.NewDecoder(body).Decode(&result)
+	fmt.Println(result)
 	return result, err
 }
 
-//SavePublicResource сохранение ресурса в папку загрузки
-func (yd YandexDisk)SavePublicResource(publikKey string) (Link, error){
-	link := "resources/download?public_key=" + publikKey
-	resp, err := yd.createPOSTRequest(link)
-	if err != nil {
-		return Link{}, err
-	}
-
-	var body []byte
-	_, err = resp.Body.Read(body)
-	if err != nil {
-		return Link{}, err
-	}
-
-	var result Link
-	err = json.Unmarshal(body, &result)
-	return result, err
-}
-
-//PublicResourcesMeta получение списка публичных файлов
-func (yd YandexDisk)PublishResourcesList(limit int, offset int) (PublicResourcesList, error){
-	link := "resources/public?limit=" + string(limit) + "&offset=" + string(offset)
-	resp, err := yd.createGETRequest(link)
-	if err != nil {
-		return PublicResourcesList{}, err
-	}
-
-	var body []byte
-	_, err = resp.Body.Read(body)
-	if err != nil {
-		return PublicResourcesList{}, err
-	}
-
-	var result PublicResourcesList
-	err = json.Unmarshal(body, &result)
-	return result, err
-}
-
-//CleanTrash Очистка файла из корзины, если path = "" происходит полная очистка
-func (yd YandexDisk)CleanTrash(path string) (Link, error){
-	link := "trash/resources?path=" + path
-	resp, err := yd.createGETRequest(link)
-	if err != nil {
-		return Link{}, err
-	}
-
-	var body []byte
-	_, err = resp.Body.Read(body)
-	if err != nil {
-		return Link{}, err
-	}
-
-	var result Link
-	err = json.Unmarshal(body, &result)
-	return result, err
-}
-
-//RestorePublicResource восстановление ранее удаленного ресурса
-//func (yd YandexDisk)RestorePublicResource() {}
+////UnpublishResource отказаться от публикации ресурса
+//func (yd YandexDisk)UnpublishResource(publickey string) (Resource, error){
+//	//TODO доделать функцию
+//	link := "public/resources" + publickey
+//	body, err := yd.createRequest(link, "GET")
+//	if err != nil {
+//		return Resource{}, err
+//	}
+//
+//	var result Resource
+//	err = json.NewDecoder(body).Decode(&result)
+//	return result, err
+//}
+//
+////PublicResourceMeta получение мета информации о публичном ресурсе
+//func (yd YandexDisk)PublicResourceMeta(publikKey string) (Resource, error){
+//	link := "public/resources?public_key=" + publikKey
+//	body, err := yd.createRequest(link, "GET")
+//	if err != nil {
+//		return Resource{}, err
+//	}
+//
+//	var result Resource
+//	err = json.NewDecoder(body).Decode(&result)
+//	return result, err
+//}
+//
+////DownloadPublicResource получение ссылки на зугрузку публичного ресурса
+//func (yd YandexDisk)DownloadPublicResource(publikKey string) (Link, error){
+//	link := "public/resources/download?public_key=" + publikKey
+//	body, err := yd.createRequest(link, "GET")
+//	if err != nil {
+//		return Link{}, err
+//	}
+//
+//	var result Link
+//	err = json.NewDecoder(body).Decode(&result)
+//	return result, err
+//}
+//
+////SavePublicResource сохранение ресурса в папку загрузки
+//func (yd YandexDisk)SavePublicResource(publikKey string) (Link, error){
+//	link := "resources/download?public_key=" + publikKey
+//	body, err := yd.createRequest(link, "POST")
+//	if err != nil {
+//		return Link{}, err
+//	}
+//
+//	var result Link
+//	err = json.NewDecoder(body).Decode(&result)
+//	return result, err
+//}
+//
+////PublicResourcesMeta получение списка публичных файлов
+//func (yd YandexDisk)PublishResourcesList(limit int, offset int) (PublicResourcesList, error){
+//	link := "resources/public?limit=" + string(limit) + "&offset=" + string(offset)
+//	body, err := yd.createRequest(link, "GET")
+//	if err != nil {
+//		return PublicResourcesList{}, err
+//	}
+//
+//	var result PublicResourcesList
+//	err = json.NewDecoder(body).Decode(&result)
+//	return result, err
+//}
+//
+////CleanTrash Очистка файла из корзины, если path = "" происходит полная очистка
+//func (yd YandexDisk)CleanTrash(path string) (Link, error){
+//	link := "trash/resources?path=" + path
+//	body, err := yd.createRequest(
+//		link,
+//		"GET",
+//		createParamsMap(map[string]string{
+//			"path": path,
+//		}),
+//		nil,
+//	)
+//	if err != nil {
+//		return Link{}, err
+//	}
+//
+//	var result Link
+//	err = json.NewDecoder(body).Decode(&result)
+//	return result, err
+//}
+//
+////RestoreResource восстановление ранее удаленного ресурса
+////func (yd YandexDisk)RestoreResource() {}
 
 //Status возвращает текущий статус выполнения операции
-func (yd YandexDisk)Status(id string) (Link, error){
+func (yd YandexDisk) Status(id string) (RequestStatus, error) {
 	link := "operations/" + id
-	resp, err := yd.createGETRequest(link)
+	body, err := yd.createRequest(
+		link,
+		"GET",
+		createParamsMap(map[string]string{}),
+		nil,
+	)
 	if err != nil {
-		return Link{}, err
+		return RequestStatus{}, err
 	}
 
-	var body []byte
-	_, err = resp.Body.Read(body)
-	if err != nil {
-		return Link{}, err
-	}
-
-	var result Link
-	err = json.Unmarshal(body, &result)
+	var result RequestStatus
+	err = json.NewDecoder(body).Decode(&result)
 	return result, err
 }
 
-func (ya YandexDisk)createGETRequest(link string) (*http.Response, error){
-	r := http.Request{}
-	r.Header.Add("Authorization", ya.accessToken)
-	return http.Get(baseURL + link)
+func (ya YandexDisk) createRequest(link, method string, form string, body io.Reader) (io.ReadCloser, error) {
+	//Формирование запроса
+	client := http.Client{}
+	req, err := http.NewRequest(method, baseURL+link+"?"+form, body)
+	if form == "" {
+		req, err = http.NewRequest(method, baseURL+link, body)
+	}
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", ya.accessToken)
+
+	//Его выполнение
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(resp.StatusCode)
+
+	return resp.Body, err
 }
 
-func (ya YandexDisk)createPOSTRequest(link string) (*http.Response, error){
-	r := http.Request{}
-	r.Header.Add("Authorization", ya.accessToken)
-	return http.Post(baseURL + link, "", nil)
-}
-
-func encodeLink(link string) string{
-	//TODO Описать тело функции
-	return link
+func createParamsMap(params map[string]string) string {
+	values := url.Values{}
+	for key, value := range params {
+		values.Add(key, value)
+	}
+	return values.Encode()
 }
--- types.go
+++ types.go
@@ -5,71 +5,76 @@ import (
 )
 
 //Link Объект содержит URL для запроса метаданных ресурса.
-type Link struct{
-	Href string `json:"href"`
-	Method string `json:"method"`
-	Templated string `json:"templated"`
+type Link struct {
+	Href      string `json:"href"`
+	Method    string `json:"method"`
+	Templated bool   `json:"templated"`
+}
+
+//GetOperationTokenВернет токен операции для проверки статуса
+func (l Link) GetOperationToken() string {
+	return l.Href[48:]
 }
 
 //Resource Описание ресурса, мета-информация о файле или папке. Включается в ответ на запрос метаинформации.
 type Resource struct {
-	Publik_key string `json:"publik_key"`
-	Embeded interface{} `json:"_embeded"`
-	Name string `json:"name"`
-	Created time.Time `json:"created"`
+	Publik_key       string            `json:"publik_key"`
+	Embeded          ResourceList      `json:"_embedded"`
+	Name             string            `json:"name"`
+	Created          time.Time         `json:"created"`
 	CustomProperties map[string]string `json:"custom_properties"`
-	PublicURL string `json:"public_url"`
-	OriginPATH string `json:"origin_path"`
-	Modified string `json:"modified"`
-	Path string `json:"path"`
-	Md5 string `json:"md_5"`
-	Type string `json:"type"`
-	Mime_type string `json:"mime_type"`
-	Size int `json:"size"`
+	PublicURL        string            `json:"public_url"`
+	OriginPATH       string            `json:"origin_path"`
+	Modified         string            `json:"modified"`
+	Path             string            `json:"path"`
+	Md5              string            `json:"md_5"`
+	Type             string            `json:"type"`
+	Mime_type        string            `json:"mime_type"`
+	Size             int               `json:"size"`
 }
 
 //ResourceList Список ресурсов, содержащихся в папке. Содержит объекты Resource и свойства списка.
 type ResourceList struct {
-	Sort string `json:"sort"`
-	Public_key string `json:"public_key"`
-	Items []Resource `json:"items"`
-	Path string `json:"path"`
-	Limit int `json:"limit"`
-	Offset int `json:"offset"`
-	Total int `json:"total"`
+	Sort       string     `json:"sort"`
+	Public_key string     `json:"public_key"`
+	Items      []Resource `json:"items"`
+	Path       string     `json:"path"`
+	Limit      int        `json:"limit"`
+	Offset     int        `json:"offset"`
+	Total      int        `json:"total"`
 }
 
 //FileResourceList Плоский список всех файлов на Диске в алфавитном порядке.
 type FileResourceList struct {
-	Items []Resource `json:"items"`
-	Limit int `json:"limit"`
-	Offset int `json:"offset"`
+	Items  []Resource `json:"items"`
+	Limit  int        `json:"limit"`
+	Offset int        `json:"offset"`
 }
 
 //LastUploadedResourceList Список последних добавленных на Диск файлов, отсортированных по дате загрузки (от поздних к ранним).
 type LastUploadedResourceList struct {
 	Items []Resource `json:"items"`
-	Limit int `json:"limit"`
+	Limit int        `json:"limit"`
 }
 
 //PublicResourcesList Список опубликованных файлов на Диске.
 type PublicResourcesList struct {
-	Items []Resource `json:"items"`
-	Type string `json:"type"`
-	Limit int `json:"limit"`
-	Offset int `json:"offset"`
+	Items  []Resource `json:"items"`
+	Type   string     `json:"type"`
+	Limit  int        `json:"limit"`
+	Offset int        `json:"offset"`
 }
 
 type systemFolders struct {
 	Applications string `json:"applications"`
-	Downloads string `json:"downloads"`
+	Downloads    string `json:"downloads"`
 }
 
 //DiskДанные о свободном и занятом пространстве на Диске
 type Disk struct {
-	Trash_size int `json:"trash_size"`
-	Total_space int `json:"total_space"`
-	User_space int `json:"user_space"`
+	Trash_size     int           `json:"trash_size"`
+	Total_space    int           `json:"total_space"`
+	User_space     int           `json:"user_space"`
 	System_folders systemFolders `json:"system_folders"`
 }
 
@@ -83,5 +88,9 @@ type Operation struct {
 //RequestError Объект для описания ошибки
 type RequestError struct {
 	Description string `json:"description"`
-	Error string `json:"error"`
+	Error       string `json:"error"`
+}
+
+type RequestStatus struct {
+	Status string `json:"status"`
 }
